f3c3995f84ea10080a35058537d82412a3354dfe
Ivan Hell <hell.ivan1991@gmail.com>
2021-08-26 10:53:32 +0200

    fix: update commander

--- src/bin/dc-outdated.ts
+++ src/bin/dc-outdated.ts
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
-import * as program from 'commander';
+import { Command } from 'commander';
 import * as os from 'os';
 import * as path from 'path';
 import { listOutdated, Options } from '../lib';
 
 const pkg = require('../../package.json');
+interface CliOptions {
+    readonly composeFile: string;
+    readonly dockerConfig: string;
+    readonly filter: string;
+    readonly excludeOfficalsAndInvalids: boolean;
+}
 
-program.version(pkg.version)
+const program = new Command().version(pkg.version)
     .option('--compose-file <file path>', 'Path to the docker-compose file. Defaults to ./docker-compose.yml')
     .option('--docker-config <file path>', 'Path to the docker config file, from which authentication details taken. Defaults to ~/.docker/config.json')
     .option('-f --filter <string>', 'Filter string to optionally filter the list of checked-images. If specified, only images-names that contain the given search string will be checked')
@@ -19,10 +25,12 @@ const options: Options = {
     dockerConfigPath: path.join(os.homedir(), '.docker', 'config.json')
 };
 
-if(program.composeFile) options.composeFilePath = program.composeFile;
-if(program.dockerConfig) options.dockerConfigPath = program.dockerConfig;
-if(program.filter) options.imagesFilter = program.filter;
-if(program.excludeOfficalsAndInvalids) options.excludeOfficalsAndInvalids = true;
+const cliOptions = program.opts<CliOptions>();
+
+if(cliOptions.composeFile) options.composeFilePath = cliOptions.composeFile;
+if(cliOptions.dockerConfig) options.dockerConfigPath = cliOptions.dockerConfig;
+if(cliOptions.filter) options.imagesFilter = cliOptions.filter;
+if(cliOptions.excludeOfficalsAndInvalids) options.excludeOfficalsAndInvalids = true;
 
 
 listOutdated(options)
